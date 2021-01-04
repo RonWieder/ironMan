1fd8edefb08f85fca55a964a3ff3aa24e9a31b73
RonWieder <ron.wider@gmail.com>
2021-01-04 13:51:25 +0200

    Fixes to d-t-p component logic

--- src/app/dyamic-table-pagination/dyamic-table-pagination.component.ts
+++ src/app/dyamic-table-pagination/dyamic-table-pagination.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { truncateSync } from 'fs';
 
 @Component({
   selector: 'iron-dyamic-table-pagination',
@@ -9,21 +10,21 @@ export class DyamicTablePaginationComponent implements OnInit {
 
   _pages: number[] = [];
   _page: number = 1;
-  _pagesNumber: number = 0;
+  _numOfPages: number = 0;
   private originalPagesArr = [];
 
   @Input() set pages(pages: number) {
     this.originalPagesArr = (new Array(pages)).fill(1).map((v, i) => i);
-    this._pagesNumber = pages;
+    this._numOfPages = pages;
     this.calculatePages();
   }
 
   get pages(): number {
-    return this._pagesNumber;
+    return this._numOfPages;
   }
 
   @Input() set currentPage(page: number) {
-    this._page = Math.max(page, 1);
+    this._page = Math.max(page + 1, 0);
     this.calculatePages();
   }
 
@@ -39,8 +40,10 @@ export class DyamicTablePaginationComponent implements OnInit {
   }
 
   private calculatePages() {
-    let minPage = Math.max(this._page - 2, 0);
-    let maxPage = Math.min(this._page + 2, this.originalPagesArr.length);
+    const startCondition: boolean = this._page === 1 || this._page === 2 ? true : false;
+    const endCondition: boolean = this._page === this._numOfPages || this._page + 1 === this._numOfPages ? true : false;
+    let minPage = startCondition ? 0 : endCondition ? this._numOfPages - 3 : Math.max(this._page - 2, 0);
+    let maxPage = endCondition ? this._numOfPages : startCondition ? 3 : Math.min(this._page + 1, this.originalPagesArr.length);
     this._pages = this.originalPagesArr.slice(minPage, maxPage);
   }
 
